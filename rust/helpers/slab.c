865d50e3b52a26456341933ca6bb8c0b54175219
Danilo Krummrich <dakr@kernel.org>
2024-08-16 02:10:48 +0200

    rust: alloc: implement `Vmalloc` allocator
    Implement `Allocator` for `Vmalloc`, the kernel's virtually contiguous
    allocator, typically used for larger objects, (much) larger than page
    size.
    
    All memory allocations made with `Vmalloc` end up in `vrealloc()`.
    
    Reviewed-by: Alice Ryhl <aliceryhl@google.com>
    Signed-off-by: Danilo Krummrich <dakr@kernel.org>
    Link: https://lore.kernel.org/r/20240816001216.26575-7-dakr@kernel.org
    [boqun: Resolve conflicts with helper split]

--- rust/helpers/slab.c
+++ rust/helpers/slab.c
@@ -1,9 +1,18 @@
 // SPDX-License-Identifier: GPL-2.0
 
 #include <linux/slab.h>
+// TODO: Move vrealloc() to a different file or rename this file.
+#include <linux/vmalloc.h>
 
 void * __must_check __realloc_size(2)
 rust_helper_krealloc(const void *objp, size_t new_size, gfp_t flags)
 {
 	return krealloc(objp, new_size, flags);
+}
+
+// TODO: Move vrealloc() to a different file or rename this file.
+void * __must_check __realloc_size(2)
+rust_helper_vrealloc(const void *p, size_t size, gfp_t flags)
+{
+	return vrealloc(p, size, flags);
 }
